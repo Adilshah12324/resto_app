a3d6b8dee9e48a9fad612a61b92e95454f623424
adil_shah <adilkhan12324@gmail.com>
2023-03-16 01:45:52 +0500

    category create and show in table done

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -28,9 +29,16 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $image = $request->file('image')->store('public/categories');
+        
+        Category::create([
+            'name' => $request->name,
+            'image' => $image,
+            'description' => $request->description,
+        ]);
+        return to_route('admin.categories.index');
     }
 
     /**
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -13,23 +13,24 @@
 
             <div class="m-2 p-2 bg-slate-300 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form action="" enctype="multipart/form-data">
+                    <form action="{{route('admin.categories.store')}}" method="post"  enctype="multipart/form-data">
+                        @csrf
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700">Name</label>
+                            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                             <div class="mt-1">
-                                <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
+                                <input type="text" id="name" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
                             </div>
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700">Image</label>
+                            <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
                             <div class="mt-1">
-                                <input type="file" id="image" wire:model.lazy="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
+                                <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
                             </div>
                         </div>
                         <div class="sm:col-span-6">
                             <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" wire:model.lazy="body" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
+                                <textarea id="description" rows="3" name="description" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400">
                                 </textarea>
                             </div>
                         </div>
